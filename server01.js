11f5025e07410a349f3e8459bb6a675c4adbdeb1
Jufebrown <jufebrown@gmail.com>
2017-03-23 09:58:03 -0500

    pasted in code from node docs to print out error

--- server01.js
+++ server01.js
@@ -3,6 +3,12 @@ const {createServer} = require(`http`)
 const server = createServer()
 
 server.on(`request`, ({url, method, headers}, res) => {
+
+  request.on('error', function(err) {
+    // This prints the error message and stack trace to `stderr`.
+    console.error(err.stack);
+  });
+
   // respond with html file
   readFile(`index.html`, (err, buff) => {
     res.end(buff)
